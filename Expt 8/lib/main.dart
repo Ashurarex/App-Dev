649adb3d5d97366dfb34950e4b81c8d5f634a290
Raghavendra <119872461+Ashurarex@users.noreply.github.com>
2025-10-16 00:17:04 +0530

    Update main.dart

--- Expt 8/lib/main.dart
+++ Expt 8/lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-
 // The main function is the entry point for all Flutter apps.
 void main() {
   runApp(const MyApp());
 }
-
 // MyApp is the root widget of the application.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,11 +22,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 // HomePage is the main screen which contains both the drawer and tab navigation.
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     // DefaultTabController is used to coordinate the TabBar and TabBarView.
@@ -121,4 +116,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
